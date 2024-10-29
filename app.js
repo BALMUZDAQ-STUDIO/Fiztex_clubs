ad9eab9f6f9094d42004c011cca5c87505c7bbdc
Kaisar <oryntaikaisar@gmail.com>
2024-10-29 12:57:28 +0500

    update

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const path = require('path');
 
 const app = express();
 
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
+
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
