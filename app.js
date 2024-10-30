a549c64c76e9da25d0b09396f6d886a422de0c96
Kaisar <oryntaikaisar@gmail.com>
2024-10-30 15:50:04 +0500

    json

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
