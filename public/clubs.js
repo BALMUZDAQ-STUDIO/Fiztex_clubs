197b25e6f22481aa364588d93cedbffd4d749532
zagadochn1y <mailybaevadilet@gmail.com>
2024-10-30 23:29:06 +0500

    club-card

--- public/clubs.js
+++ public/clubs.js
@@ -1,12 +1,21 @@
 async function getData() {
     const response = await fetch("../data/clubs.json");
-    const data = await response.json(); // Добавлен await перед response.json()
-    return data; // Теперь возвращаем данные
+    const data = await response.json();
+    return data;
 }
 
-async function main() {
-    const data = await getData(); // Добавлено await для получения результата
+function createClubs(data) {
+    const listClubs = Object.entries(data).map(item => {
+        `<div class = "club-card"><club-card imgSrc = "${item['image']}" clubName = "${item['name']}" aboutClub = "${item['description']}"></club-card></div>`
+    }).join(' ');
+    document.getElementById('club-cards').innerHTML = listClubs;
     console.log(data);
 }
 
+
+async function main() {
+    const data = await getData();
+    console.log(data);
+    await createClubs(data);
+}
 main();
