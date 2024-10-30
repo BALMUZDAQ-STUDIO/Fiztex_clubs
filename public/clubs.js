d207cf9d5f623e808b7528132fd544d22507c5e2
BALMUSDAQ-STUDIO <balmuzdaq.studio@gmail.com>
2024-10-30 23:53:14 +0500

    data -> public

--- public/clubs.js
+++ public/clubs.js
@@ -1,21 +1,26 @@
 async function getData() {
-    const response = await fetch("../data/clubs.json");
+    const response = await fetch("data/clubs.json");
     const data = await response.json();
     return data;
 }
 
 function createClubs(data) {
-    const listClubs = Object.entries(data).map(item => {
-        `<div class = "club-card"><club-card imgSrc = "${item['image']}" clubName = "${item['name']}" aboutClub = "${item['description']}"></club-card></div>`
-    }).join(' ');
-    document.getElementById('club-cards').innerHTML = listClubs;
+    const listClubs = Object.entries(data)
+        .map(([key, item]) => {
+            return `<div class="club-card">
+                    <club-card imgSrc="${item.image}" clubName="${item.name}" aboutClub="${item.description}">
+                    </club-card>
+                </div>`;
+        })
+        .join(" ");
+
+    document.getElementById("club-cards").innerHTML = listClubs;
     console.log(data);
 }
-
 
 async function main() {
     const data = await getData();
     console.log(data);
-    await createClubs(data);
+    createClubs(data);
 }
 main();
