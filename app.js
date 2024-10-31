037f9877f482ad9b5981677bdeed3ba6c413ac70
Kaisar <oryntaikaisar@gmail.com>
2024-10-31 22:55:06 +0500

    fix

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const expressSession = require('express-session');
-const cookieParser = require('cookie-parser');
 const path = require('path');
 
 const app = express();
 
-SECRET = process.env.SECRET||'qwerty';
 
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
-app.use(cookieParser(SECRET));
-app.use(expressSession({
-    secret: SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        maxAge: 60 * 60 * 1000
-    }
-}));
 
 app.get('/', (req, res, err) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -27,15 +15,7 @@ app.get('/clubs', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'clubs.html'));
 });
 
-app.get('/set-classes', (req, res) => {
-    req.session.test = 'abcd';
-    res.send('Test');
-});
 
-app.get('/get-classes', (req, res) => {
-    let test = req.session.test;
-    console.log(test);
-});
 
 PORT = process.env.PORT || 3000;
 
