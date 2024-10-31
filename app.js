e8d77ecf0d9a7fe524f7ab1a5652df4a4d320a8c
Kaisar <oryntaikaisar@gmail.com>
2024-10-31 16:58:41 +0500

    cookie

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const expressSession = require('express-session');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.json());
-app.use(cookieParser());
+app.use(expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000
+    }
+}))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
